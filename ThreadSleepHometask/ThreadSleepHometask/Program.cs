cea0c44ea532efaed975f188694826885c3233a2
Don <trade400@gmail.com>
2014-10-20 20:44:28 +0300

    Add ThreadSleepHometask and ThreadingTrueFalseNull

---
+++ ThreadSleepHometask/ThreadSleepHometask/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadSleepHometask
+{
+    /*1. Создать несколько потоков, выводящих на консоль в цикле свой hashcode. Посмотреть порядок вывода.
+        Добавить Sleep(1), Sleep(10), Sleep(0). Посмотреть изменения в порядке вывода.  
+     * Закоментировать Sleep() и установить приоритеты потокам. Посмотреть изменения в порядке вывода.*/
+    class Program
+    {
+        static void Print(object o)
+        {
+            Console.WriteLine("Поток {0}",o);
+            for (; ; ) { }
+        }
+
+        static void Main(string[] args)
+        {
+            int flag = 0;
+
+            ParameterizedThreadStart p = new ParameterizedThreadStart(Print);
+            Thread th1 = new Thread(p);
+            Thread th2 = new Thread(p);
+            Thread th3 = new Thread(p);
+            Thread th4 = new Thread(p);
+            Thread th5 = new Thread(p);
+            th1.Priority = ThreadPriority.Lowest;
+            th2.Priority = ThreadPriority.Lowest;
+            th3.Priority = ThreadPriority.Lowest;
+            th4.Priority = ThreadPriority.Highest;
+            th5.Priority = ThreadPriority.Lowest;
+            th1.Start(1);
+            th2.Start(2);
+            th3.Start(3);
+            th4.Start(4);
+            th5.Start(5);
+
+            do
+            {
+                //Thread.Sleep(0);
+                //Thread.Sleep(10);
+                //Thread.Sleep(1);
+                Console.WriteLine("----" + flag + "----");
+                if (Thread.CurrentThread.IsAlive)
+                    Console.WriteLine("Main thread "+Thread.CurrentThread.GetHashCode());
+                if (th1.IsAlive)
+                    Console.WriteLine("Thread 1 " + th1.GetHashCode());
+                if (th2.IsAlive)
+                    Console.WriteLine("Thread 2 " + th2.GetHashCode());
+                if (th3.IsAlive)
+                    Console.WriteLine("Thread 3 " + th3.GetHashCode());
+                if (th4.IsAlive)
+                    Console.WriteLine("Thread 4 " + th4.GetHashCode());
+                if (th5.IsAlive)
+                    Console.WriteLine("Thread 5 " + th5.GetHashCode());
+                
+                flag++;
+
+            } while (flag<5000);
+            Console.WriteLine("The End");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThreadingTrueFalseNull/Threading/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threading
+{
+    class UserClass
+    {
+        Queue<bool?> q = new Queue<bool?>();
+       
+        public void TrueFalseNull(object o)
+        {
+            bool? flag;
+            do
+            {
+                Console.WriteLine();
+                int a = 0;
+                Thread.Sleep(100);
+                Random r = new Random();
+                Thread.Sleep(100);
+                a = r.Next(1, 4);
+                if (a == 1)
+                {
+                    flag = true;
+                    
+                }
+                if (a == 2)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    flag = null;
+                    Console.WriteLine("Поток" + (int)o);
+                    Console.WriteLine("NULL");
+                    Console.WriteLine();
+                }
+
+                if (flag != null)
+                {
+                    q.Enqueue(flag);
+                    Console.WriteLine("Поток" + (int)o);
+                    Console.WriteLine("{0} записан в очередь", flag);
+                    Console.WriteLine();
+                }
+                
+            } while (flag != null);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserClass uObj = new UserClass();
+
+            ParameterizedThreadStart param = new ParameterizedThreadStart(uObj.TrueFalseNull);
+
+            Thread t1 = new Thread(param);
+            Thread t2 = new Thread(param);
+            Thread t3 = new Thread(param);
+
+            t1.Start(1);
+            t2.Start(2);
+            t3.Start(3);
+            Thread.Sleep(2000);
+            if (!t1.IsAlive)
+                Console.WriteLine("Поток 1 завершён");
+            if (!t2.IsAlive)
+                Console.WriteLine("Поток 2 завершён");
+            if (!t3.IsAlive)
+                Console.WriteLine("Поток 3 завершён"); 
+            Console.ReadKey();
+            
+        }
+    }
+}
